86108c17a21b6fe8b2b98ae105e20244d24d42b1
OMG-link <50469469+OMG-link@users.noreply.github.com>
2023-03-19 21:04:50 +0800

    [New] 新增Driver类
    Driver类是实际运行编译流水线的对象

---
+++ src/main/java/com/bit/newnewcc/Driver.java
@@ -0,0 +1,18 @@
+package com.bit.newnewcc;
+
+/**
+ * 实际运行编译流水线的对象
+ */
+public class Driver {
+
+    private final CompileOption compileOption;
+
+    public Driver(CompileOption compileOption){
+        this.compileOption = compileOption;
+    }
+
+    public void launch(){
+
+    }
+
+}
