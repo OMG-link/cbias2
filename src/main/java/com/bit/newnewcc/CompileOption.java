f45323fa362d3dac40968a64d2d24f8fa0d2bd0a
OMG-link <50469469+OMG-link@users.noreply.github.com>
2023-03-21 14:09:49 +0800

    [Update] 增加编译过程检查选项

--- src/main/java/com/bit/newnewcc/CompileOption.java
+++ src/main/java/com/bit/newnewcc/CompileOption.java
@@ -4,20 +4,50 @@ package com.bit.newnewcc;
  * 编译器设置
  */
 public class CompileOption {
+
+    /**
+     * 输出文件的类型，包括：LLVM IR、ASM
+     */
     public enum OutputType{
         LLVM_IR, ASM
     }
 
+    /**
+     * 优化等级，包括：
+     * <br>
+     * - O0: 完全按照输入进行翻译 <br>
+     * - O1: 进行基本的优化 <br>
+     * - O2: 进行高阶的优化 <br>
+     */
     public enum OptimizeLevel{
         O0,O1,O2
     }
 
+    /**
+     * 源代码文件
+     */
     public String sourceFile;
 
+    /**
+     * 输出文件类型
+     */
     public OutputType outputType = OutputType.ASM;
+    /**
+     * 输出文件名
+     */
     public String outputFile = "a.out";
 
+    /**
+     * 优化等级
+     */
     public OptimizeLevel optimizeLevel = OptimizeLevel.O1;
+
+    /**
+     * 是否启用严格的编译过程检查 <br>
+     * 当该值为true时，编译的每一个步骤后都会检查当前代码的引用关系是否有效，适用于调试过程 <br>
+     * 正式编译时，不要开启此选项，以免拖慢编译速度 <br>
+     */
+    public boolean enableStrictCompileProcessCheck = false;
 
     /**
      * 由命令行参数生成一个编译器设置
@@ -37,6 +67,9 @@ public class CompileOption {
                     }
                     case "emit-llvm" -> {
                         compileOption.outputType = CompileOption.OutputType.LLVM_IR;
+                    }
+                    case "check-compile-process"-> {
+                        compileOption.enableStrictCompileProcessCheck = true;
                     }
                     case "O0" -> {
                         compileOption.optimizeLevel = CompileOption.OptimizeLevel.O0;
@@ -63,7 +96,8 @@ public class CompileOption {
                 ", outputType=" + outputType +
                 ", outputFile='" + outputFile + '\'' +
                 ", optimizeLevel=" + optimizeLevel +
+                ", enableStrictCompileProcessCheck=" + enableStrictCompileProcessCheck +
                 '}';
     }
-    
+
 }
