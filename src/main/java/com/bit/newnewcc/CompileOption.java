65f176620f429fa469c45fb7cc66bf21e8466bd2
wtz <wangtz0607@outlook.com>
2023-03-27 23:30:42 +0800

    docs: 改进文档格式

--- src/main/java/com/bit/newnewcc/CompileOption.java
+++ src/main/java/com/bit/newnewcc/CompileOption.java
@@ -1,59 +1,62 @@
 package com.bit.newnewcc;
 
 /**
- * 编译器设置
+ * 编译器选项。
  */
 public class CompileOption {
     /**
-     * 输出文件的类型，包括：LLVM IR、ASM
+     * 输出文件类型。
      */
     public enum OutputType {
         LLVM_IR, ASM
     }
 
     /**
-     * 优化等级，包括：
-     * <br>
-     * - O0: 完全按照输入进行翻译 <br>
-     * - O1: 进行基本的优化 <br>
-     * - O2: 进行高阶的优化 <br>
+     * 优化等级。
      */
     public enum OptimizeLevel {
-        O0, O1, O2
+        /** 完全按照输入翻译。 */
+        O0,
+        /** 基本优化。 */
+        O1,
+        /** 高阶优化。 */
+        O2,
     }
 
     /**
-     * 源代码文件
+     * 源文件名。
      */
     public String sourceFile;
 
     /**
-     * 输出文件类型
+     * 输出文件类型。
      */
     public OutputType outputType = OutputType.ASM;
 
     /**
-     * 输出文件名
+     * 输出文件名。
      */
     public String outputFile = "a.out";
 
     /**
-     * 优化等级
+     * 优化等级。
      */
     public OptimizeLevel optimizeLevel = OptimizeLevel.O1;
 
     /**
-     * 是否启用严格的编译过程检查 <br>
-     * 当该值为true时，编译的每一个步骤后都会检查当前代码的引用关系是否有效，适用于调试过程 <br>
-     * 正式编译时，不要开启此选项，以免拖慢编译速度 <br>
+     * 是否启用严格的编译过程检查。
+     * <p>
+     * 当该值为true时，编译的每一个步骤后都会检查当前代码的引用关系是否有效，适用于调试过程。
+     * <p>
+     * 请勿在正式编译时开启此选项，以免拖慢编译速度。
      */
     public boolean enableStrictCompileProcessCheck = false;
 
     /**
-     * 由命令行参数生成一个编译器设置
+     * 根据命令行参数构造编译器选项。
      *
      * @param args 命令行参数
-     * @return 命令行参数对应的编译器设置
+     * @return 命令行参数对应的编译器选项
      */
     public static CompileOption fromCmdArguments(String[] args) {
         var compileOption = new CompileOption();
--- src/main/java/com/bit/newnewcc/Driver.java
+++ src/main/java/com/bit/newnewcc/Driver.java
@@ -1,7 +1,7 @@
 package com.bit.newnewcc;
 
 /**
- * 实际运行编译流水线的对象
+ * 实际运行编译流水线的对象。
  */
 public class Driver {
     private final CompileOption compileOption;
