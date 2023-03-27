24d80d09859e75889bc6bc4c8277ebd6cc885886
wtz <wangtz0607@outlook.com>
2023-03-27 23:38:57 +0800

    style: 改进代码格式

--- src/main/java/com/bit/newnewcc/Driver.java
+++ src/main/java/com/bit/newnewcc/Driver.java
@@ -6,11 +6,11 @@ package com.bit.newnewcc;
 public class Driver {
     private final CompilerOptions compilerOptions;
 
-    public Driver(CompilerOptions compilerOptions){
+    public Driver(CompilerOptions compilerOptions) {
         this.compilerOptions = compilerOptions;
     }
 
-    public void launch(){
+    public void launch() {
 
     }
 }
