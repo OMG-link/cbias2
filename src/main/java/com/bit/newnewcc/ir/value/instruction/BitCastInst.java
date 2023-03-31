f5047ae8b8f44e8aefab3c5545fefeda84072c6d
OMG-link <50469469+OMG-link@users.noreply.github.com>
2023-03-31 18:05:51 +0800

    feat: bitcast指令

---
+++ src/main/java/com/bit/newnewcc/ir/value/instruction/BitCastInst.java
@@ -0,0 +1,54 @@
+package com.bit.newnewcc.ir.value.instruction;
+
+import com.bit.newnewcc.ir.Operand;
+import com.bit.newnewcc.ir.Type;
+import com.bit.newnewcc.ir.Value;
+import com.bit.newnewcc.ir.value.Instruction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 位转换指令 <br>
+ * 此指令可以强制将一个类型转化为另一个类型，而不改变内存结构 <br>
+ * 在本项目中，其唯一用途是调用memset时需要将float*转化为i32* <br>
+ * @see <a href="https://llvm.org/docs/LangRef.html#bitcast-to-instruction">LLVM IR文档</a>
+ */
+public class BitCastInst extends Instruction {
+    private final Operand sourceOperand;
+    public BitCastInst(Type sourceType, Type targetType){
+        super(targetType);
+        this.sourceOperand = new Operand(this,sourceType,null);
+    }
+
+    public BitCastInst(Value sourceValue, Type targetType){
+        super(targetType);
+        this.sourceOperand = new Operand(this,sourceValue.getType(),sourceValue);
+    }
+
+    public Value getSourceOperand() {
+        return sourceOperand.getValue();
+    }
+
+    public void setSourceOperand(Value value){
+        sourceOperand.setValue(value);
+    }
+
+    @Override
+    public List<Operand> getOperandList() {
+        var list = new ArrayList<Operand>();
+        list.add(sourceOperand);
+        return list;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "%s = bitcast %s %s to %s",
+                this.getValueName(),
+                getSourceOperand().getTypeName(),
+                getSourceOperand().getValueName(),
+                this.getTypeName()
+        );
+    }
+}
