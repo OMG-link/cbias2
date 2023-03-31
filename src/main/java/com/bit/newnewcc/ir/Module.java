44bff69f84e2d68482bd74e9cd0f31dc9b0f1175
OMG-link <50469469+OMG-link@users.noreply.github.com>
2023-03-31 16:22:33 +0800

    feat: 模块

--- src/main/java/com/bit/newnewcc/ir/Module.java
+++ src/main/java/com/bit/newnewcc/ir/Module.java
@@ -1,17 +1,49 @@
 package com.bit.newnewcc.ir;
 
+import com.bit.newnewcc.ir.exception.IllegalArgumentException;
+import com.bit.newnewcc.ir.value.ExternalFunction;
 import com.bit.newnewcc.ir.value.Function;
+import com.bit.newnewcc.ir.value.GlobalVariable;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 
 /**
- * 模块
- * <br>
+ * 模块 <br>
  * 每个源文件构成一个模块
  */
 public class Module {
-    private final Collection<Function> functions = new ArrayList<>();
-    //private final Collection<GlobalVariable> globalVariables = new ArrayList<GlobalVariable>();
+    private final Collection<Function> functions = new HashSet<>();
+    private final Collection<ExternalFunction> externalFunctions = new HashSet<>();
+    private final Collection<GlobalVariable> globalVariables = new HashSet<>();
+
+    public void addFunction(Function function) {
+        functions.add(function);
+    }
+
+    public Collection<Function> getFunctions() {
+        return Collections.unmodifiableCollection(functions);
+    }
+
+    public void addExternalFunction(ExternalFunction externalFunction) {
+        if (externalFunction instanceof Function) {
+            throw new IllegalArgumentException("Cannot insert function as an external function.");
+        }
+        externalFunctions.add(externalFunction);
+    }
+
+    public Collection<ExternalFunction> getExternalFunctions() {
+        return Collections.unmodifiableCollection(externalFunctions);
+    }
+
+    public void addGlobalVariable(GlobalVariable globalVariable) {
+        globalVariables.add(globalVariable);
+    }
+
+    public Collection<GlobalVariable> getGlobalVariables() {
+        return Collections.unmodifiableCollection(globalVariables);
+    }
 
 }
