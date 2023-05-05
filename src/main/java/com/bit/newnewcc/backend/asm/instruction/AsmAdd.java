771a1aaab386b5e6821b6e70a22e375623154f38
fallleaves01 <adoubiq@qq.com>
2023-05-05 12:19:35 +0800

    feat:增加了add指令的建模

---
+++ src/main/java/com/bit/newnewcc/backend/asm/instruction/AsmAdd.java
@@ -0,0 +1,23 @@
+package com.bit.newnewcc.backend.asm.instruction;
+
+import com.bit.newnewcc.backend.asm.operand.AsmOperand;
+import com.bit.newnewcc.backend.asm.operand.Register;
+
+/**
+ * 汇编加指令，分为普通加和加立即数两种
+ */
+public class AsmAdd extends AsmInstruction {
+    /**
+     * 汇编加指令
+     *
+     * @param goal    结果存储的寄存器
+     * @param source1 源寄存器1，存储第一个加数
+     * @param source2 源2，存储第二个加数，可能为寄存器或立即数
+     */
+    public AsmAdd(Register goal, Register source1, AsmOperand source2) {
+        super("add", goal, source1, source2);
+        if (source2.isImmediate() || source2.isGlobalTag()) {
+            setInstructionName("addi");
+        }
+    }
+}
