67f5ca00d40023994493c1b88a1ff85dfdba7958
OMG-link <50469469+OMG-link@users.noreply.github.com>
2023-05-10 23:13:51 +0800

    feat: 增加Driver流程

--- src/main/java/cn/edu/bit/newnewcc/Driver.java
+++ src/main/java/cn/edu/bit/newnewcc/Driver.java
@@ -4,12 +4,16 @@ import cn.edu.bit.newnewcc.frontend.SysYLexer;
 import cn.edu.bit.newnewcc.frontend.SysYParser;
 import cn.edu.bit.newnewcc.frontend.Translator;
 import cn.edu.bit.newnewcc.ir.Module;
+import cn.edu.bit.newnewcc.ir.util.IREmitter;
+import cn.edu.bit.newnewcc.pass.IrPassManager;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class Driver {
     private final CompilerOptions compilerOptions;
@@ -28,6 +32,15 @@ public class Driver {
             Translator visitor = new Translator();
             visitor.visit(tree);
             Module module = visitor.getModule();
+
+            // 在IR层面优化代码
+            IrPassManager.optimize(module, compilerOptions.getOptimizationLevel());
+            // 输出LLVM IR格式的文件
+            if (compilerOptions.isEmitLLVM()) {
+                try (var fileOutputStream = new FileOutputStream(compilerOptions.getOutputFileName())) {
+                    fileOutputStream.write(IREmitter.emit(module).getBytes(StandardCharsets.UTF_8));
+                }
+            }
         }
     }
 }
