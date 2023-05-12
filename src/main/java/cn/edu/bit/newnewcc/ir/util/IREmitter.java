9250dcb31a24099e17be7849798e97566294c50f
OMG-link <50469469+OMG-link@users.noreply.github.com>
2023-05-12 17:23:11 +0800

    fix: 修正全局变量IR输出

--- src/main/java/cn/edu/bit/newnewcc/ir/util/IREmitter.java
+++ src/main/java/cn/edu/bit/newnewcc/ir/util/IREmitter.java
@@ -87,7 +87,7 @@ public class IREmitter {
                 "%s = dso_local %s %s %s\n",
                 globalVariable.getValueNameIR(),
                 globalVariable.isConstant() ? "constant" : "global",
-                globalVariable.getTypeName(),
+                globalVariable.getType().getBaseType().getTypeName(),
                 globalVariable.getInitialValue().getValueNameIR()
         ));
     }
