a3820e0f66a6dd3d1acfde5f8f1555105abd8328
OMG-link <50469469+OMG-link@users.noreply.github.com>
2023-05-23 15:09:43 +0800

    feat: 移动语义检查的位置

--- src/main/java/cn/edu/bit/newnewcc/pass/IrPassManager.java
+++ src/main/java/cn/edu/bit/newnewcc/pass/IrPassManager.java
@@ -6,6 +6,7 @@ import cn.edu.bit.newnewcc.pass.ir.*;
 public class IrPassManager {
 
     public static void optimize(Module module, int optimizeLevel) {
+        IrSemanticCheckPass.verify(module);
         switch (optimizeLevel) {
             case 0 -> {
             }
@@ -13,16 +14,15 @@ public class IrPassManager {
                 MemoryToRegisterPass.runOnModule(module);
                 while (true) {
                     boolean changed = false;
-                    changed = changed | PatternReplacementPass.runOnModule(module);
-                    changed = changed | ConstantFoldingPass.runOnModule(module);
-                    changed = changed | BranchSimplifyPass.runOnModule(module);
-                    changed = changed | BasicBlockMergePass.runOnModule(module);
-                    changed = changed | DeadCodeEliminationPass.runOnModule(module);
+                    changed |= PatternReplacementPass.runOnModule(module);
+                    changed |= ConstantFoldingPass.runOnModule(module);
+                    changed |= BranchSimplifyPass.runOnModule(module);
+                    changed |= BasicBlockMergePass.runOnModule(module);
+                    changed |= DeadCodeEliminationPass.runOnModule(module);
                     if (!changed) break;
                 }
             }
         }
-        IrSemanticCheckPass.verify(module);
     }
 
 }
