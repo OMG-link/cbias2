b34dc9d434eb80baafc56a9b8e1008e87afb5989
OMG-link <50469469+OMG-link@users.noreply.github.com>
2023-05-23 17:20:00 +0800

    feat: 增强无效代码删除中对于地址类型的判断
    现在的判断更准确了

--- src/main/java/cn/edu/bit/newnewcc/pass/ir/DeadCodeEliminationPass.java
+++ src/main/java/cn/edu/bit/newnewcc/pass/ir/DeadCodeEliminationPass.java
@@ -9,7 +9,6 @@ import cn.edu.bit.newnewcc.ir.value.*;
 import cn.edu.bit.newnewcc.ir.value.instruction.*;
 
 import java.util.*;
-import java.util.function.Consumer;
 
 /**
  * 消除 ir 中的无用代码
@@ -45,134 +44,204 @@ import java.util.function.Consumer;
 // 缩写：se = side effect
 public class DeadCodeEliminationPass {
 
-    private static Collection<Instruction> getSeInstructions(Module module) {
-        Set<Instruction> reachableInstructions = new HashSet<>();
-        Set<Instruction> seInstructions = new HashSet<>();
-        Queue<Function> queue = new ArrayDeque<>();
-        var addSeInstruction = new Consumer<Instruction>() {
-            private final Set<Function> pushedFunctions = new HashSet<>();
+    /**
+     * 用于高效分析地址是否为全局变量 <br>
+     */
+    // 思路如下：
+    // 每当遇到一个待定的值，先向上搜索其所有可能的来源，并将所有值标记为待定（pending）
+    // 若搜索到了全局源，则从全局源开始向下传播全局地址标记
+    // 最后，没有被标记的待定值都是局部地址，因为其找不到任何全局源
+    private static class AddressTypeCache {
 
-            @Override
-            public void accept(Instruction instruction) {
-                seInstructions.add(instruction);
-                var function = instruction.getBasicBlock().getFunction();
-                if (!pushedFunctions.contains(function)) {
-                    pushedFunctions.add(function);
-                    queue.add(function);
-                }
+        private final Map<Value, Boolean> addressTypeCache = new HashMap<>();
+        private final Queue<Value> spreadingGlobalAddresses = new ArrayDeque<>();
+        private final Set<Value> pendingAddresses = new HashSet<>();
+
+        // 确定了address为全局地址
+        private void addGlobalAddress(Value address) {
+            if (addressTypeCache.containsKey(address)) {
+                assert addressTypeCache.get(address);
+                return;
             }
-        };
-        var searchBasicBlock = new Consumer<BasicBlock>() {
-            private final Set<BasicBlock> visitedBlocks = new HashSet<>();
+            addressTypeCache.put(address, true);
+            pendingAddresses.remove(address);
+            spreadingGlobalAddresses.add(address);
+        }
 
-            private boolean isBasicSeInstruction(Instruction instruction) {
-                // 1. 从函数入口块可达的存储地址为全局变量的store指令
-                if (instruction instanceof StoreInst storeInst) {
-                    if (isGlobalAddress(storeInst.getAddressOperand())) {
-                        return true;
+        /**
+         * 向上搜索地址源头的过程
+         *
+         * @param address 待搜索的地址
+         */
+        // 将所有搜索到的值定义为pending状态
+        //
+        // 若当前搜索到的是：
+        // 确定的全局变量：调用addGlobalAddress
+        // 确定的局部变量：什么也不做，全部搜索完毕后会将pending状态的所有值标记为局部变量
+        // 由其他地址确定的语句：对其他地址调用judgeAddress
+        private void judgeAddress(Value address) {
+            if (addressTypeCache.containsKey(address)) return;
+            if (pendingAddresses.contains(address)) return;
+            pendingAddresses.add(address);
+            if (address instanceof GlobalVariable || address instanceof Function.FormalParameter) {
+                addGlobalAddress(address);
+            } else if (address instanceof AllocateInst) {
+                // 是局部变量，什么也不做
+            } else if (address instanceof GetElementPtrInst getElementPtrInst) {
+                judgeAddress(getElementPtrInst.getRootOperand());
+            } else if (address instanceof PhiInst phiInst) {
+                for (BasicBlock block : phiInst.getEntrySet()) {
+                    judgeAddress(phiInst.getValue(block));
+                }
+            } else {
+                throw new IllegalArgumentException(String.format("Cannot analysis variable of type %s.", address.getClass()));
+            }
+        }
+
+        public boolean isGlobalAddress(Value address) {
+            if (!addressTypeCache.containsKey(address)) {
+                judgeAddress(address);
+                while (!spreadingGlobalAddresses.isEmpty()) {
+                    var globalAddress = spreadingGlobalAddresses.remove();
+                    // 向下传播全局地址标记
+                    for (Operand usage : globalAddress.getUsages()) {
+                        var userInstruction = usage.getInstruction();
+                        // 传播到所有 返回值是地址 且 受全局地址影响 的语句上
+                        if (userInstruction instanceof GetElementPtrInst || userInstruction instanceof PhiInst) {
+                            addGlobalAddress(userInstruction);
+                        }
                     }
                 }
-                // 2. 从函数入口块可达的调用外部函数的指令
-                if (instruction instanceof CallInst callInst) {
-                    if (callInst.getCallee() instanceof ExternalFunction) {
-                        return true;
-                    }
+                for (Value pendingAddress : pendingAddresses) {
+                    addressTypeCache.put(pendingAddress, false);
                 }
-                return false;
+                pendingAddresses.clear();
             }
+            assert addressTypeCache.containsKey(address);
+            assert spreadingGlobalAddresses.isEmpty();
+            assert pendingAddresses.isEmpty();
+            return addressTypeCache.get(address);
+        }
 
-            @Override
-            public void accept(BasicBlock basicBlock) {
-                if (visitedBlocks.contains(basicBlock)) return;
-                visitedBlocks.add(basicBlock);
-                for (Instruction instruction : basicBlock.getInstructions()) {
-                    if (isBasicSeInstruction(instruction)) {
-                        addSeInstruction.accept(instruction);
-                    }
-                    reachableInstructions.add(instruction);
-                }
-                for (BasicBlock exitBlock : basicBlock.getExitBlocks()) {
-                    accept(exitBlock);
-                }
+    }
+
+    AddressTypeCache addressTypeCache = new AddressTypeCache();
+
+    /// 可达指令 & 副作用指令搜索
+
+    private final Set<Instruction> reachableInstructions = new HashSet<>();
+    private final Set<BasicBlock> reachableBlocks = new HashSet<>();
+    private final Set<Instruction> seInstructions = new HashSet<>();
+    private final Set<Function> seFunctions = new HashSet<>();
+    Queue<Function> seFunctionQueue = new ArrayDeque<>();
+
+    private boolean isBasicSeInstruction(Instruction instruction) {
+        // 1. 从函数入口块可达的存储地址为全局变量的store指令
+        if (instruction instanceof StoreInst storeInst) {
+            if (addressTypeCache.isGlobalAddress(storeInst.getAddressOperand())) {
+                return true;
             }
-        };
-        module.getFunctions().forEach(function -> searchBasicBlock.accept(function.getEntryBasicBlock()));
-        while (!queue.isEmpty()) {
-            var function = queue.remove();
+        }
+        // 2. 从函数入口块可达的调用外部函数的指令
+        if (instruction instanceof CallInst callInst) {
+            if (callInst.getCallee() instanceof ExternalFunction) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void addSeInstruction(Instruction instruction) {
+        seInstructions.add(instruction);
+        var function = instruction.getBasicBlock().getFunction();
+        if (!seFunctions.contains(function)) {
+            seFunctions.add(function);
+            seFunctionQueue.add(function);
+        }
+    }
+
+    private void dfsForReachableInstructions(BasicBlock basicBlock) {
+        if (reachableBlocks.contains(basicBlock)) return;
+        reachableBlocks.add(basicBlock);
+        for (Instruction instruction : basicBlock.getInstructions()) {
+            if (isBasicSeInstruction(instruction)) {
+                addSeInstruction(instruction);
+            }
+            reachableInstructions.add(instruction);
+        }
+        for (BasicBlock exitBlock : basicBlock.getExitBlocks()) {
+            dfsForReachableInstructions(exitBlock);
+        }
+    }
+
+    private void findSeInstructions(Module module) {
+        for (Function function : module.getFunctions()) {
+            dfsForReachableInstructions(function.getEntryBasicBlock());
+        }
+        while (!seFunctionQueue.isEmpty()) {
+            var function = seFunctionQueue.remove();
             for (Operand operand : function.getUsages()) {
                 if (reachableInstructions.contains(operand.getInstruction())) {
                     // 为什么要检查这么多呢？谁知道会出现什么奇怪的情况，还是限制死了不留坑
                     if (operand.getInstruction() instanceof CallInst callInst && callInst.getCallee() == function) {
-                        addSeInstruction.accept(callInst);
+                        addSeInstruction(callInst);
                     }
                 }
             }
         }
-        return seInstructions;
     }
 
-    private static Collection<Instruction> getValidInstructions(Module module, Collection<Instruction> seInstructions) {
-        Set<Instruction> reachableInstructions = new HashSet<>();
-        Set<Instruction> validInstructions = new HashSet<>();
-        Queue<Instruction> queue = new ArrayDeque<>();
-        Consumer<Instruction> addValidInstruction = instruction -> {
-            if (!validInstructions.contains(instruction)) {
-                validInstructions.add(instruction);
-                queue.add(instruction);
-            }
-        };
-        var searchBasicBlock = new Consumer<BasicBlock>() {
-            private final Set<BasicBlock> visitedBlocks = new HashSet<>();
+    /// 有效指令搜索
 
-            private boolean isBasicValidInstruction(Instruction instruction) {
-                // 1. 从函数入口块可达的副作用指令
-                if (seInstructions.contains(instruction)) {
-                    return true;
-                }
-                // 2. 从函数入口块可达的存储地址为局部变量的store指令
-                if (instruction instanceof StoreInst storeInst) {
-                    var address = storeInst.getAddressOperand();
-                    if (!isGlobalAddress(address)) {
-                        return true;
-                    }
-                }
-                // 3. 从函数入口块可达的控制流指令
-                if (instruction instanceof TerminateInst) {
-                    return true;
-                }
-                return false;
-            }
+    private final Set<Instruction> validInstructions = new HashSet<>();
+    private final Queue<Instruction> validInstructionQueue = new ArrayDeque<>();
 
-            @Override
-            public void accept(BasicBlock basicBlock) {
-                if (visitedBlocks.contains(basicBlock)) return;
-                visitedBlocks.add(basicBlock);
-                for (Instruction instruction : basicBlock.getInstructions()) {
-                    if (isBasicValidInstruction(instruction)) {
-                        addValidInstruction.accept(instruction);
-                    }
-                    reachableInstructions.add(instruction);
-                }
-                for (BasicBlock exitBlock : basicBlock.getExitBlocks()) {
-                    accept(exitBlock);
-                }
+    private boolean isBasicValidInstruction(Instruction instruction) {
+        // 1. 从函数入口块可达的副作用指令
+        if (seInstructions.contains(instruction)) {
+            return true;
+        }
+        // 2. 从函数入口块可达的存储地址为局部变量的store指令
+        if (instruction instanceof StoreInst storeInst) {
+            var address = storeInst.getAddressOperand();
+            if (!addressTypeCache.isGlobalAddress(address)) {
+                return true;
             }
-        };
-        module.getFunctions().forEach(function -> searchBasicBlock.accept(function.getEntryBasicBlock()));
-        while (!queue.isEmpty()) {
-            var instruction = queue.remove();
-            for (Operand operand : instruction.getOperandList()) {
-                if (operand.getValue() instanceof Instruction instruction1) {
-                    if (reachableInstructions.contains(instruction1)) {
-                        addValidInstruction.accept(instruction1);
-                    }
+        }
+        // 3. 从函数入口块可达的控制流指令
+        if (instruction instanceof TerminateInst) {
+            return true;
+        }
+        return false;
+    }
+
+    private void addValidInstruction(Instruction instruction) {
+        if (!validInstructions.contains(instruction)) {
+            validInstructions.add(instruction);
+            validInstructionQueue.add(instruction);
+        }
+    }
+
+    private void findValidInstructions() {
+        for (BasicBlock reachableBlock : reachableBlocks) {
+            for (Instruction instruction : reachableBlock.getInstructions()) {
+                if (isBasicValidInstruction(instruction)) {
+                    addValidInstruction(instruction);
                 }
             }
         }
-        return validInstructions;
+        while (!validInstructionQueue.isEmpty()) {
+            var validInstruction = validInstructionQueue.remove();
+            for (Operand operand : validInstruction.getOperandList()) {
+                // sValidInstruction = spread valid instruction
+                if (operand.getValue() instanceof Instruction sValidInstruction && reachableInstructions.contains(sValidInstruction)) {
+                    addValidInstruction(sValidInstruction);
+                }
+            }
+        }
     }
 
-    private static boolean removeInvalidInstruction(Module module, Collection<Instruction> validInstructions) {
+    private boolean removeInvalidInstruction(Module module) {
         var invalidInstructions = new ArrayList<Instruction>();
         var invalidBasicBlocks = new ArrayList<BasicBlock>();
         // 获取以上两个数组
@@ -221,24 +290,14 @@ public class DeadCodeEliminationPass {
         return invalidInstructions.size() > 0 || invalidBasicBlocks.size() > 0;
     }
 
-    private static boolean isGlobalAddress(Value address) {
-        boolean answer;
-        if (address instanceof GlobalVariable || address instanceof Function.FormalParameter) {
-            answer = true;
-        } else if (address instanceof AllocateInst) {
-            answer = false;
-        } else if (address instanceof GetElementPtrInst getElementPtrInst) {
-            answer = isGlobalAddress(getElementPtrInst.getRootOperand());
-        } else {
-            throw new IllegalArgumentException(String.format("Cannot analysis variable of type %s.", address.getClass()));
-        }
-        return answer;
+    private boolean runOnModule_(Module module) {
+        findSeInstructions(module);
+        findValidInstructions();
+        return removeInvalidInstruction(module);
     }
 
     public static boolean runOnModule(Module module) {
-        var seInstructions = getSeInstructions(module);
-        var validInstructions = getValidInstructions(module, seInstructions);
-        return removeInvalidInstruction(module, validInstructions);
+        return new DeadCodeEliminationPass().runOnModule_(module);
     }
 
 }
