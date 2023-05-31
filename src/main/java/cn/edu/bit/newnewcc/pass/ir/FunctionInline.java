03d7b8451bcf681107864deb30d6d3c8bc6f4edd
OMG-link <50469469+OMG-link@users.noreply.github.com>
2023-06-01 00:07:33 +0800

    feat: 函数内联
    未测试，因此未加入优化池

---
+++ src/main/java/cn/edu/bit/newnewcc/pass/ir/FunctionInline.java
@@ -0,0 +1,99 @@
+package cn.edu.bit.newnewcc.pass.ir;
+
+import cn.edu.bit.newnewcc.ir.Module;
+import cn.edu.bit.newnewcc.ir.Operand;
+import cn.edu.bit.newnewcc.ir.value.BasicBlock;
+import cn.edu.bit.newnewcc.ir.value.Function;
+import cn.edu.bit.newnewcc.ir.value.Instruction;
+import cn.edu.bit.newnewcc.ir.value.instruction.CallInst;
+import cn.edu.bit.newnewcc.ir.value.instruction.JumpInst;
+import cn.edu.bit.newnewcc.ir.value.instruction.PhiInst;
+import cn.edu.bit.newnewcc.pass.ir.structure.FunctionClone;
+
+import java.util.*;
+
+public class FunctionInline {
+
+    record FunctionProperty(int size, Set<Function> callees) {
+
+    }
+
+    private final Module module;
+    private final Map<Function, FunctionProperty> propertyMap = new HashMap<>();
+
+    private FunctionInline(Module module) {
+        this.module = module;
+    }
+
+    /**
+     * 收集相关信息
+     */
+    private void collectInformation() {
+        for (Function function : module.getFunctions()) {
+            Set<Function> callees = new HashSet<>();
+            int size = 0;
+            for (BasicBlock basicBlock : function.getBasicBlocks()) {
+                for (Instruction mainInstruction : basicBlock.getMainInstructions()) {
+                    if (mainInstruction instanceof CallInst callInst) {
+                        if (callInst.getCallee() instanceof Function callee) {
+                            callees.add(callee);
+                        }
+                    }
+                    size++;
+                }
+            }
+            propertyMap.put(function, new FunctionProperty(size, callees));
+        }
+    }
+
+    private void inlineFunction(Function function) {
+        for (Operand usage : function.getUsages()) {
+            if (!(usage.getInstruction() instanceof CallInst callInst && callInst.getCallee() == function)) continue;
+            // 将原有基本块按照 call 指令的位置拆分为两个
+            var blockAlpha = callInst.getBasicBlock();
+            var blockBeta = new BasicBlock();
+            boolean startMoving = false;
+            for (Instruction instruction : blockAlpha.getMainInstructions()) {
+                if (startMoving) {
+                    instruction.removeFromBasicBlock();
+                    blockBeta.addInstruction(instruction);
+                }
+                if (instruction == callInst) startMoving = true;
+            }
+            var betaTerminate = blockAlpha.getTerminateInstruction();
+            betaTerminate.removeFromBasicBlock();
+            blockBeta.setTerminateInstruction(betaTerminate);
+            for (Operand blockAlphaUsage : blockAlpha.getUsages()) {
+                if (blockAlphaUsage.getInstruction() instanceof PhiInst) {
+                    blockAlphaUsage.setValue(blockBeta);
+                }
+            }
+            // 插入内联后的函数
+            var clonedFunction = new FunctionClone(function, callInst.getArgumentList(), blockBeta);
+            blockAlpha.setTerminateInstruction(new JumpInst(clonedFunction.getEntryBlock()));
+            for (BasicBlock clonedBlock : clonedFunction.getBasicBlocks()) {
+                function.addBasicBlock(clonedBlock);
+            }
+            callInst.replaceAllUsageTo(clonedFunction.getReturnValue());
+        }
+    }
+
+    private boolean runOnModule() {
+        boolean changed = false;
+        collectInformation();
+        List<Function> functions = new ArrayList<>(module.getFunctions());
+        functions.sort((function1, function2) -> Integer.compare(propertyMap.get(function1).size, propertyMap.get(function2).size));
+        for (Function inlinedFunction : functions) {
+            if (propertyMap.get(inlinedFunction).callees.contains(inlinedFunction)) continue;
+            inlineFunction(inlinedFunction);
+            module.removeFunction(inlinedFunction);
+            changed = true;
+        }
+        return changed;
+    }
+
+    public static boolean runOnModule(Module module) {
+        return new FunctionInline(module).runOnModule();
+    }
+
+}
---
+++ src/main/java/cn/edu/bit/newnewcc/pass/ir/structure/FunctionClone.java
@@ -0,0 +1,268 @@
+package cn.edu.bit.newnewcc.pass.ir.structure;
+
+import cn.edu.bit.newnewcc.ir.Type;
+import cn.edu.bit.newnewcc.ir.Value;
+import cn.edu.bit.newnewcc.ir.exception.IllegalArgumentException;
+import cn.edu.bit.newnewcc.ir.exception.IllegalStateException;
+import cn.edu.bit.newnewcc.ir.value.*;
+import cn.edu.bit.newnewcc.ir.value.instruction.*;
+
+import java.util.*;
+
+/**
+ * 函数的克隆体 <br>
+ * 函数的实参、返回地址等均用符号代替，需使用replaceAllUsageTo方法替换 <br>
+ */
+public class FunctionClone {
+
+    private class Symbol extends Value {
+        public Symbol(Type type) {
+            super(type);
+        }
+
+        private String name;
+
+        @Override
+        public String getValueName() {
+            if (name == null) {
+                name = String.format("%s#%s", this.getClass(), UUID.randomUUID());
+            }
+            return name;
+        }
+
+        @Override
+        public String getValueNameIR() {
+            return '%' + getValueName();
+        }
+
+        @Override
+        public void setValueName(String valueName) {
+            this.name = name;
+        }
+
+    }
+
+    private final Function function;
+    private final Map<Value, Value> valueMap = new HashMap<>();
+    private final BasicBlock returnBlock;
+    private final List<BasicBlock> basicBlocks = new ArrayList<>();
+    private final PhiInst returnValue;
+
+    public FunctionClone(Function function, List<Value> arguments, BasicBlock returnBlock) {
+        this.function = function;
+        int parameterNum = function.getFormalParameters().size();
+        for (int i = 0; i < parameterNum; i++) {
+            this.valueMap.put(function.getFormalParameters().get(i), arguments.get(i));
+        }
+        this.returnBlock = returnBlock;
+        this.returnValue = new PhiInst(function.getReturnType());
+        doFunctionClone();
+    }
+
+    public BasicBlock getEntryBlock() {
+        return (BasicBlock) valueMap.get(function.getEntryBasicBlock());
+    }
+
+    public List<BasicBlock> getBasicBlocks() {
+        return basicBlocks;
+    }
+
+    public PhiInst getReturnValue() {
+        return returnValue;
+    }
+
+    private static boolean isGlobalValue(Value value) {
+        return value instanceof GlobalVariable || value instanceof BaseFunction;
+    }
+
+    private Value getReplacedValue(Value value) {
+        if (isGlobalValue(value)) {
+            return value;
+        } else {
+            if (!valueMap.containsKey(value)) {
+                throw new IllegalStateException("No local value were found to replace.");
+            } else {
+                return valueMap.get(value);
+            }
+        }
+    }
+
+    private void setValueMapKv(Value key, Value value) {
+        valueMap.get(key).replaceAllUsageTo(value);
+        valueMap.put(key, value);
+    }
+
+    private Instruction cloneInstruction(Instruction instruction) {
+        if (instruction instanceof AllocateInst allocateInst) {
+            return new AllocateInst(allocateInst.getAllocatedType());
+        } else if (instruction instanceof BitCastInst bitCastInst) {
+            return new BitCastInst(
+                    getReplacedValue(bitCastInst.getSourceOperand()),
+                    bitCastInst.getTargetType()
+            );
+        } else if (instruction instanceof BranchInst branchInst) {
+            return new BranchInst(
+                    getReplacedValue(branchInst.getCondition()),
+                    (BasicBlock) getReplacedValue(branchInst.getTrueExit()),
+                    (BasicBlock) getReplacedValue(branchInst.getFalseExit())
+            );
+        } else if (instruction instanceof CallInst callInst) {
+            var argumentList = new ArrayList<Value>();
+            for (int i = 0; i < callInst.getArgumentSize(); i++) {
+                argumentList.add(getReplacedValue(callInst.getArgumentAt(i)));
+            }
+            return new CallInst(
+                    (BaseFunction) getReplacedValue(callInst.getCallee()),
+                    argumentList
+            );
+        } else if (instruction instanceof FloatAddInst floatAddInst) {
+            return new FloatAddInst(
+                    floatAddInst.getType(),
+                    getReplacedValue(floatAddInst.getOperand1()),
+                    getReplacedValue(floatAddInst.getOperand2())
+            );
+        } else if (instruction instanceof FloatCompareInst floatCompareInst) {
+            return new FloatCompareInst(
+                    floatCompareInst.getComparedType(),
+                    floatCompareInst.getCondition(),
+                    getReplacedValue(floatCompareInst.getOperand1()),
+                    getReplacedValue(floatCompareInst.getOperand2())
+            );
+        } else if (instruction instanceof FloatDivideInst floatDivideInst) {
+            return new FloatAddInst(
+                    floatDivideInst.getType(),
+                    getReplacedValue(floatDivideInst.getOperand1()),
+                    getReplacedValue(floatDivideInst.getOperand2())
+            );
+        } else if (instruction instanceof FloatMultiplyInst floatMultiplyInst) {
+            return new FloatAddInst(
+                    floatMultiplyInst.getType(),
+                    getReplacedValue(floatMultiplyInst.getOperand1()),
+                    getReplacedValue(floatMultiplyInst.getOperand2())
+            );
+        } else if (instruction instanceof FloatNegateInst floatNegateInst) {
+            return new FloatNegateInst(
+                    floatNegateInst.getType(),
+                    floatNegateInst.getOperand1()
+            );
+        } else if (instruction instanceof FloatSubInst floatSubInst) {
+            return new FloatAddInst(
+                    floatSubInst.getType(),
+                    getReplacedValue(floatSubInst.getOperand1()),
+                    getReplacedValue(floatSubInst.getOperand2())
+            );
+        } else if (instruction instanceof FloatToSignedIntegerInst floatToSignedIntegerInst) {
+            return new FloatToSignedIntegerInst(
+                    getReplacedValue(floatToSignedIntegerInst.getSourceOperand()),
+                    floatToSignedIntegerInst.getTargetType()
+            );
+        } else if (instruction instanceof GetElementPtrInst getElementPtrInst) {
+            var indices = new ArrayList<Value>();
+            for (Value indexOperand : getElementPtrInst.getIndexOperands()) {
+                indices.add(getReplacedValue(indexOperand));
+            }
+            return new GetElementPtrInst(
+                    getReplacedValue(getElementPtrInst.getRootOperand()),
+                    indices
+            );
+        } else if (instruction instanceof IntegerAddInst integerAddInst) {
+            return new IntegerAddInst(
+                    integerAddInst.getType(),
+                    getReplacedValue(integerAddInst.getOperand1()),
+                    getReplacedValue(integerAddInst.getOperand2())
+            );
+        } else if (instruction instanceof IntegerCompareInst integerCompareInst) {
+            return new IntegerCompareInst(
+                    integerCompareInst.getComparedType(),
+                    integerCompareInst.getCondition(),
+                    getReplacedValue(integerCompareInst.getOperand1()),
+                    getReplacedValue(integerCompareInst.getOperand2())
+            );
+        } else if (instruction instanceof IntegerMultiplyInst integerMultiplyInst) {
+            return new IntegerAddInst(
+                    integerMultiplyInst.getType(),
+                    getReplacedValue(integerMultiplyInst.getOperand1()),
+                    getReplacedValue(integerMultiplyInst.getOperand2())
+            );
+        } else if (instruction instanceof IntegerSignedDivideInst integerSignedDivideInst) {
+            return new IntegerAddInst(
+                    integerSignedDivideInst.getType(),
+                    getReplacedValue(integerSignedDivideInst.getOperand1()),
+                    getReplacedValue(integerSignedDivideInst.getOperand2())
+            );
+        } else if (instruction instanceof IntegerSignedRemainderInst integerSignedRemainderInst) {
+            return new IntegerAddInst(
+                    integerSignedRemainderInst.getType(),
+                    getReplacedValue(integerSignedRemainderInst.getOperand1()),
+                    getReplacedValue(integerSignedRemainderInst.getOperand2())
+            );
+        } else if (instruction instanceof IntegerSubInst integerSubInst) {
+            return new IntegerAddInst(
+                    integerSubInst.getType(),
+                    getReplacedValue(integerSubInst.getOperand1()),
+                    getReplacedValue(integerSubInst.getOperand2())
+            );
+        } else if (instruction instanceof JumpInst jumpInst) {
+            return new JumpInst((BasicBlock) getReplacedValue(jumpInst.getExit()));
+        } else if (instruction instanceof LoadInst loadInst) {
+            return new LoadInst(getReplacedValue(loadInst.getAddressOperand()));
+        } else if (instruction instanceof PhiInst phiInst) {
+            var clonedPhiInst = new PhiInst(phiInst.getType());
+            for (BasicBlock entryBlock : phiInst.getEntrySet()) {
+                clonedPhiInst.setValue(
+                        (BasicBlock) getReplacedValue(entryBlock),
+                        getReplacedValue(phiInst.getValue(entryBlock))
+                );
+            }
+            return clonedPhiInst;
+        } else if (instruction instanceof ReturnInst) {
+            return new JumpInst(returnBlock);
+        } else if (instruction instanceof SignedIntegerToFloatInst signedIntegerToFloatInst) {
+            return new SignedIntegerToFloatInst(
+                    getReplacedValue(signedIntegerToFloatInst.getSourceOperand()),
+                    signedIntegerToFloatInst.getTargetType()
+            );
+        } else if (instruction instanceof StoreInst storeInst) {
+            return new StoreInst(
+                    getReplacedValue(storeInst.getAddressOperand()),
+                    getReplacedValue(storeInst.getValueOperand())
+            );
+        } else if (instruction instanceof ZeroExtensionInst zeroExtensionInst) {
+            return new ZeroExtensionInst(
+                    getReplacedValue(zeroExtensionInst.getSourceOperand()),
+                    zeroExtensionInst.getTargetType()
+            );
+        } else {
+            throw new IllegalArgumentException("Cannot clone instruction of type " + instruction.getClass());
+        }
+    }
+
+    private void doFunctionClone() {
+        // 构建占位符
+        for (BasicBlock basicBlock : function.getBasicBlocks()) {
+            valueMap.put(basicBlock, new Symbol(basicBlock.getType()));
+            for (Instruction instruction : basicBlock.getInstructions()) {
+                valueMap.put(instruction, new Symbol(instruction.getType()));
+            }
+        }
+        // 构建函数体
+        for (BasicBlock basicBlock : function.getBasicBlocks()) {
+            var clonedBlock = new BasicBlock();
+            setValueMapKv(basicBlock, clonedBlock);
+            for (Instruction instruction : basicBlock.getInstructions()) {
+                var clonedInstruction = cloneInstruction(instruction);
+                setValueMapKv(instruction, clonedInstruction);
+                if (instruction instanceof ReturnInst returnInst) {
+                    returnValue.setValue(clonedBlock, getReplacedValue(returnInst.getReturnValue()));
+                }
+                if (instruction instanceof AllocateInst) {
+                    returnBlock.getFunction().getEntryBasicBlock().addInstruction(clonedInstruction);
+                } else {
+                    clonedBlock.addInstruction(clonedInstruction);
+                }
+            }
+            this.basicBlocks.add(clonedBlock);
+        }
+    }
+
+}
