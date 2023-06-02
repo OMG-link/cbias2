857e5b7e4e96d2a976448cdb89e1b0a5bb733f19
fallleaves01 <adoubiq@qq.com>
2023-06-02 19:34:04 +0800

    feat:增加了整数比较指令的展开及翻译

--- src/main/java/cn/edu/bit/newnewcc/backend/asm/instruction/AsmIntegerCompare.java
+++ src/main/java/cn/edu/bit/newnewcc/backend/asm/instruction/AsmIntegerCompare.java
@@ -1,6 +1,7 @@
 package cn.edu.bit.newnewcc.backend.asm.instruction;
 
 import cn.edu.bit.newnewcc.backend.asm.operand.AsmOperand;
+import cn.edu.bit.newnewcc.backend.asm.operand.Immediate;
 import cn.edu.bit.newnewcc.backend.asm.operand.IntRegister;
 import cn.edu.bit.newnewcc.ir.value.instruction.IntegerCompareInst;
 
@@ -13,9 +14,19 @@ public class AsmIntegerCompare extends AsmInstruction {
         super(null, goal, rsource1, source2);
         this.condition = condition;
         switch (condition) {
-            case SEQZ -> setInstructionName("seqz");
-            case SNEZ -> setInstructionName("snez");
-            case SLT -> setInstructionName("slt");
+            case SEQZ:
+                setInstructionName("seqz");
+                break;
+            case SNEZ:
+                setInstructionName("snez");
+                break;
+            case SLT:
+                if (source2 instanceof Immediate) {
+                    setInstructionName("slti");
+                } else {
+                    setInstructionName("slt");
+                }
+                break;
         }
     }
 }
