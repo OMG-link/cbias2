73b4783f945bfe400d748f2356632088005dd2a1
fallleaves01 <adoubiq@qq.com>
2023-06-04 13:39:47 +0800

    feat:新增了AddressTag类，用于和寻址操作中的地址进行区分

--- src/main/java/cn/edu/bit/newnewcc/backend/asm/operand/AddressTag.java
+++ src/main/java/cn/edu/bit/newnewcc/backend/asm/operand/AddressTag.java
@@ -1,5 +1,8 @@
 package cn.edu.bit.newnewcc.backend.asm.operand;
 
+/**
+ * 地址标记类，类似于指针，通常在指令中被视为立即数，可以和地址类之间互相转换
+ */
 public class AddressTag extends AsmOperand {
     private final long offset;
     private final IntRegister baseAddress;
