35fe7dc6629ebe29c6834e86086e914708900597
wtz <wangtz0607@outlook.com>
2023-06-24 00:07:55 +0800

    refactor: 修正变量命名

--- src/main/java/cn/edu/bit/newnewcc/Main.java
+++ src/main/java/cn/edu/bit/newnewcc/Main.java
@@ -7,15 +7,15 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws IOException {
         String outputFilename = null;
-        int optimizeLevel = 0;
+        int optimizationLevel = 0;
         boolean emitAssembly = false;
         boolean emitLLVM = false;
         List<String> inputFilenames = new ArrayList<>();
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
                 case "-o" -> outputFilename = args[++i];
-                case "-O0" -> optimizeLevel = 0;
-                case "-O1", "-O2" -> optimizeLevel = 1;
+                case "-O0" -> optimizationLevel = 0;
+                case "-O1", "-O2" -> optimizationLevel = 1;
                 case "-S" -> emitAssembly = true;
                 case "--emit-llvm" -> emitLLVM = true;
                 default -> inputFilenames.add(args[i]);
@@ -23,12 +23,13 @@ public class Main {
         }
 
         Driver driver = new Driver(
-                new CompilerOptions(
-                        inputFilenames.toArray(new String[0]),
-                        outputFilename, optimizeLevel,
-                        emitAssembly,
-                        emitLLVM
-                )
+            new CompilerOptions(
+                inputFilenames.toArray(new String[0]),
+                outputFilename,
+                optimizationLevel,
+                emitAssembly,
+                emitLLVM
+            )
         );
         driver.launch();
     }
