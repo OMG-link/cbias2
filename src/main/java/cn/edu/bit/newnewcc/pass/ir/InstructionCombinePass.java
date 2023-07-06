5c849a5ae7905f6eafca17ba050f6d81c18056db
OMG-link <50469469+OMG-link@users.noreply.github.com>
2023-07-06 19:42:43 +0800

    feat: 增强 InstructionCombinePass

--- src/main/java/cn/edu/bit/newnewcc/pass/ir/InstructionCombinePass.java
+++ src/main/java/cn/edu/bit/newnewcc/pass/ir/InstructionCombinePass.java
@@ -4,14 +4,21 @@ import cn.edu.bit.newnewcc.ir.Module;
 import cn.edu.bit.newnewcc.ir.value.BasicBlock;
 import cn.edu.bit.newnewcc.ir.value.Function;
 import cn.edu.bit.newnewcc.ir.value.Instruction;
+import cn.edu.bit.newnewcc.pass.ir.structure.DomTree;
 import cn.edu.bit.newnewcc.pass.ir.structure.InstructionSet;
 
 /**
  * 指令合并 <br>
- * 合并同一基本块内的等效指令 <br>
+ * 根据支配关系删去多余指令 <br>
  */
 public class InstructionCombinePass {
-    private static boolean runOnBasicBlock(BasicBlock basicBlock) {
+    private InstructionCombinePass() {
+    }
+
+    private final InstructionSet instructionSet = new InstructionSet();
+    private DomTree domTree;
+
+    private boolean runOnBasicBlock(BasicBlock basicBlock) {
         boolean changed = false;
         var instructionSet = new InstructionSet();
         for (Instruction instruction : basicBlock.getInstructions()) {
@@ -26,12 +33,36 @@ public class InstructionCombinePass {
         return changed;
     }
 
+    private boolean dfsDomTree(BasicBlock basicBlock) {
+        boolean changed = false;
+        for (Instruction instruction : basicBlock.getInstructions()) {
+            if (instructionSet.contains(instruction)) {
+                instruction.replaceAllUsageTo(instructionSet.get(instruction));
+                instruction.waste();
+                changed = true;
+            } else {
+                instructionSet.add(instruction);
+            }
+        }
+        for (BasicBlock domSon : domTree.getDomSons(basicBlock)) {
+            changed |= dfsDomTree(domSon);
+        }
+        for (Instruction instruction : basicBlock.getInstructions()) {
+            instructionSet.remove(instruction);
+        }
+        return changed;
+    }
+
+    private boolean runOnFunction(Function function) {
+        domTree = new DomTree(function);
+        return dfsDomTree(function.getEntryBasicBlock());
+    }
+
     public static boolean runOnModule(Module module) {
         boolean changed = false;
         for (Function function : module.getFunctions()) {
-            for (BasicBlock basicBlock : function.getBasicBlocks()) {
-                changed |= runOnBasicBlock(basicBlock);
-            }
+            var passInstance = new InstructionCombinePass();
+            changed |= passInstance.runOnFunction(function);
         }
         return changed;
     }
