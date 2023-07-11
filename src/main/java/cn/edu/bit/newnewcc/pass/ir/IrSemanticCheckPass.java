155ddbd4e49641884189b280cbfe939b9860ed0f
OMG-link <50469469+OMG-link@users.noreply.github.com>
2023-07-11 22:38:11 +0800

    feat: 增强IR语义检查
    IR语义检查中的可用局部变量改为由支配关系确定

--- src/main/java/cn/edu/bit/newnewcc/pass/ir/IrSemanticCheckPass.java
+++ src/main/java/cn/edu/bit/newnewcc/pass/ir/IrSemanticCheckPass.java
@@ -9,6 +9,7 @@ import cn.edu.bit.newnewcc.ir.value.Function;
 import cn.edu.bit.newnewcc.ir.value.Instruction;
 import cn.edu.bit.newnewcc.ir.value.instruction.AllocateInst;
 import cn.edu.bit.newnewcc.ir.value.instruction.PhiInst;
+import cn.edu.bit.newnewcc.pass.ir.structure.DomTree;
 import cn.edu.bit.newnewcc.pass.ir.util.UtilFunctions;
 
 import java.util.*;
@@ -23,6 +24,7 @@ import java.util.*;
 public class IrSemanticCheckPass {
 
     private final Set<Value> globalValues = new HashSet<>();
+    private DomTree domTree;
     private Set<Value> localValues;
     private Map<BasicBlock, Set<BasicBlock>> basicBlockEntries;
 
@@ -31,29 +33,30 @@ public class IrSemanticCheckPass {
 
     private void verifyInstruction(Instruction instruction) {
         // 检查操作数合法性
-        for (Operand operand : instruction.getOperandList()) {
-            if (!operand.hasValueBound()) {
-                throw new SemanticCheckFailedException("Operand has no value bound");
-            }
-            if (!(UtilFunctions.isGlobalValue(operand.getValue()) || localValues.contains(operand.getValue()))) {
-                throw new SemanticCheckFailedException("Value bound cannot be used as an operand");
+        // Phi 指令的实际使用位置是每个入口块末尾，因此不在这里检查（Phi指令检查的localValues不是此时的）
+        if (!(instruction instanceof PhiInst)) {
+            for (Operand operand : instruction.getOperandList()) {
+                if (!operand.hasValueBound()) {
+                    throw new SemanticCheckFailedException("Operand has no value bound");
+                }
+                if (!(UtilFunctions.isGlobalValue(operand.getValue()) || localValues.contains(operand.getValue()))) {
+                    throw new SemanticCheckFailedException("Value bound cannot be used as an operand");
+                }
             }
         }
         // 检查使用该语句值的语句合法性
         for (Operand usage : instruction.getUsages()) {
-            if (!localValues.contains(usage.getInstruction())) {
-                System.out.printf("In %s, it is used by %s, which is in basic block %s.\n",
+            if (usage.getInstruction().getBasicBlock() == null) {
+                System.out.printf("In %s, it is used by %s, which is not in any block.\n",
                         instruction.getClass(),
-                        usage.getInstruction().getClass(),
-                        usage.getInstruction().getBasicBlock()
+                        usage.getInstruction().getClass()
                 );
                 throw new SemanticCheckFailedException("Value being used by a free instruction.");
             }
         }
     }
 
-    private void verifyBasicBlock(BasicBlock basicBlock, boolean isFunctionEntry) {
-        basicBlock.getInstructions().forEach(this::verifyInstruction);
+    private void verifyBlocksOverDomTree(BasicBlock basicBlock, boolean isFunctionEntry) {
         basicBlock.getLeadingInstructions().forEach(instruction -> {
             if (instruction instanceof AllocateInst && !isFunctionEntry) {
                 throw new SemanticCheckFailedException("Alloca instruction must be placed in entry block");
@@ -62,6 +65,29 @@ public class IrSemanticCheckPass {
         if (basicBlock.getTerminateInstruction() == null) {
             throw new SemanticCheckFailedException("Basic block has no terminate instruction");
         }
+        for (Instruction instruction : basicBlock.getInstructions()) {
+            localValues.add(instruction);
+            verifyInstruction(instruction);
+        }
+        // 检查Phi指令
+        for (Operand usage : basicBlock.getUsages()) {
+            if (usage.getInstruction() instanceof PhiInst phiInst) {
+                if (!phiInst.hasEntry(basicBlock)) {
+                    throw new SemanticCheckFailedException("One-way usage binding.");
+                }
+                var value = phiInst.getValue(basicBlock);
+                if (value == null) {
+                    throw new SemanticCheckFailedException("Operand has no value bound.");
+                }
+                if (!(UtilFunctions.isGlobalValue(value) || localValues.contains(value))) {
+                    throw new SemanticCheckFailedException("Value bound cannot be used as an operand");
+                }
+            }
+        }
+        for (BasicBlock domSon : domTree.getDomSons(basicBlock)) {
+            verifyBlocksOverDomTree(domSon, false);
+        }
+        basicBlock.getInstructions().forEach(localValues::remove);
     }
 
     private void verifyFunction(Function function) {
@@ -69,13 +95,8 @@ public class IrSemanticCheckPass {
         localValues = new HashSet<>(globalValues);
         localValues.addAll(function.getFormalParameters());
         localValues.addAll(function.getBasicBlocks());
-        for (BasicBlock basicBlock : function.getBasicBlocks()) {
-            localValues.addAll(basicBlock.getInstructions());
-        }
-        // 检查每个基本块是否正常
-        for (BasicBlock basicBlock : function.getBasicBlocks()) {
-            verifyBasicBlock(basicBlock, basicBlock == function.getEntryBasicBlock());
-        }
+        domTree = DomTree.buildOver(function);
+        verifyBlocksOverDomTree(function.getEntryBasicBlock(), true);
         // 用 getExitBlocks 方法收集块入口列表
         basicBlockEntries = new HashMap<>();
         for (BasicBlock basicBlock : function.getBasicBlocks()) {
