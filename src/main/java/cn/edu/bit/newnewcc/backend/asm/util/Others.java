1ff53641dd9e2abb417b6e2aaf025eb655e4fc60
fallleaves01 <adoubiq@qq.com>
2023-08-02 17:13:30 +0800

    feat:增加了虚拟寄存器指令检查，用于判断非ssa情况出现

--- src/main/java/cn/edu/bit/newnewcc/backend/asm/util/Others.java
+++ src/main/java/cn/edu/bit/newnewcc/backend/asm/util/Others.java
@@ -1,5 +1,11 @@
 package cn.edu.bit.newnewcc.backend.asm.util;
 
+import cn.edu.bit.newnewcc.backend.asm.controller.LifeTimeController;
+import cn.edu.bit.newnewcc.backend.asm.instruction.AsmInstruction;
+
+import java.util.List;
+import java.util.Objects;
+
 public class Others {
     public static String deleteCharString(String sourceString, String charTable) {
         StringBuilder res = new StringBuilder();
@@ -10,5 +16,14 @@ public class Others {
             }
         }
         return res.toString();
+    }
+    public static void check(List<AsmInstruction> instructionList) {
+        for (var inst : instructionList) {
+            for (var x : LifeTimeController.getReadVregSet(inst)) {
+                for (var y : LifeTimeController.getWriteVregSet(inst)) {
+                    assert !x.equals(y);
+                }
+            }
+        }
     }
 }
