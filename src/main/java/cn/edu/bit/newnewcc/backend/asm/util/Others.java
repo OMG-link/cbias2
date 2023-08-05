cb0ef801903dc492052cb8e5ff8ce40209ab28cd
OMG-link <50469469+OMG-link@users.noreply.github.com>
2023-08-06 00:30:31 +0800

    feat: 指令选择时，常数除法转乘法

--- src/main/java/cn/edu/bit/newnewcc/backend/asm/util/Others.java
+++ src/main/java/cn/edu/bit/newnewcc/backend/asm/util/Others.java
@@ -1,13 +1,8 @@
 package cn.edu.bit.newnewcc.backend.asm.util;
 
-import cn.edu.bit.newnewcc.backend.asm.controller.LifeTimeController;
 import cn.edu.bit.newnewcc.backend.asm.instruction.AsmInstruction;
-import cn.edu.bit.newnewcc.backend.asm.instruction.AsmLoad;
-import cn.edu.bit.newnewcc.backend.asm.instruction.AsmStore;
-import cn.edu.bit.newnewcc.backend.asm.operand.Register;
 
 import java.util.List;
-import java.util.Objects;
 
 public class Others {
     public static String deleteCharString(String sourceString, String charTable) {
@@ -20,6 +15,7 @@ public class Others {
         }
         return res.toString();
     }
+
     public static void check(List<AsmInstruction> instructionList) {
         for (var inst : instructionList) {
             for (var x : inst.getReadVRegSet()) {
@@ -31,4 +27,19 @@ public class Others {
             }
         }
     }
+
+    public static boolean isPowerOf2(int x) {
+        return x > 0 && ((x & (x - 1)) == 0);
+    }
+
+    public static int log2(int x) {
+        if (x <= 0) return -1;
+        int count = 0;
+        while (x > 1) {
+            count++;
+            x >>= 1;
+        }
+        return count;
+    }
+
 }
