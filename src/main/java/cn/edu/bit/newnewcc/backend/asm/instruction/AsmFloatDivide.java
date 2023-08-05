4ecac66abcfe2ff243a47ed7afdd34cbba4405e6
wtz <wangtz0607@outlook.com>
2023-08-06 02:26:43 +0800

    refactor: 重构AsmFloatDivide

--- src/main/java/cn/edu/bit/newnewcc/backend/asm/instruction/AsmFloatDivide.java
+++ src/main/java/cn/edu/bit/newnewcc/backend/asm/instruction/AsmFloatDivide.java
@@ -4,6 +4,11 @@ import cn.edu.bit.newnewcc.backend.asm.operand.FloatRegister;
 
 public class AsmFloatDivide extends AsmInstruction {
     public AsmFloatDivide(FloatRegister dest, FloatRegister source1, FloatRegister source2) {
-        super("fdiv.s", dest, source1, source2);
+        super("", dest, source1, source2);
+    }
+
+    @Override
+    public String emit() {
+        return String.format("\tfdiv.s %s, %s, %s\n", getOperand(1), getOperand(2), getOperand(3));
     }
 }
