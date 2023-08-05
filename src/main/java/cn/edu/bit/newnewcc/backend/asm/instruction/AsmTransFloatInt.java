8834ca667edb75b96c2e648431a805cb64b5d175
wtz <wangtz0607@outlook.com>
2023-08-06 02:27:11 +0800

    refactor: 重构AsmTransFloatInt

--- src/main/java/cn/edu/bit/newnewcc/backend/asm/instruction/AsmTransFloatInt.java
+++ src/main/java/cn/edu/bit/newnewcc/backend/asm/instruction/AsmTransFloatInt.java
@@ -1,14 +1,45 @@
 package cn.edu.bit.newnewcc.backend.asm.instruction;
 
-import cn.edu.bit.newnewcc.backend.asm.operand.AsmRTZ;
 import cn.edu.bit.newnewcc.backend.asm.operand.FloatRegister;
 import cn.edu.bit.newnewcc.backend.asm.operand.IntRegister;
 
 public class AsmTransFloatInt extends AsmInstruction {
-    public AsmTransFloatInt(IntRegister dest, FloatRegister source) {
-        super("fcvt.w.s", dest, source, new AsmRTZ());
+    public enum Opcode {
+        FCVTWS("fcvt.w.s"),
+        FCVTSW("fcvt.s.w");
+
+        private final String name;
+
+        Opcode(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
+
+    private final Opcode opcode;
+
+    public AsmTransFloatInt(IntRegister dest, FloatRegister source) {
+        super("", dest, source, null);
+        opcode = Opcode.FCVTWS;
+    }
+
     public AsmTransFloatInt(FloatRegister dest, IntRegister source) {
-        super("fcvt.s.w", dest, source, null);
+        super("", dest, source, null);
+        opcode = Opcode.FCVTSW;
+    }
+
+    public Opcode getOpcode() {
+        return opcode;
+    }
+
+    @Override
+    public String emit() {
+        if (opcode == Opcode.FCVTWS)
+            return String.format("\t%s %s, %s, rtz\n", getOpcode().getName(), getOperand(1), getOperand(2));
+        else
+            return String.format("\t%s %s, %s\n", getOpcode().getName(), getOperand(1), getOperand(2));
     }
 }
