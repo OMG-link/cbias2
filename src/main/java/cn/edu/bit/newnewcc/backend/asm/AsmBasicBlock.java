884191de6cd701e4aa8612d2a7a28ebe7a11976b
fallleaves01 <adoubiq@qq.com>
2023-08-06 13:46:59 +0800

    fix:增加了AsmBlockEnd作为基本块结束占位符

--- src/main/java/cn/edu/bit/newnewcc/backend/asm/AsmBasicBlock.java
+++ src/main/java/cn/edu/bit/newnewcc/backend/asm/AsmBasicBlock.java
@@ -51,6 +51,7 @@ public class AsmBasicBlock {
         for (Instruction instruction : irBlock.getInstructions()) {
             translate(instruction);
         }
+        function.appendInstruction(new AsmBlockEnd());
     }
 
     /**
