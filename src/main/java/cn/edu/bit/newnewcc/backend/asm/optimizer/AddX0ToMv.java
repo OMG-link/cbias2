8daad5d6a0f3d54e0bff7992125cf4a28ae6dbb9
wtz <wangtz0607@outlook.com>
2023-08-07 12:54:08 +0800

    feat: 添加AddX0ToMv

---
+++ src/main/java/cn/edu/bit/newnewcc/backend/asm/optimizer/AddX0ToMv.java
@@ -0,0 +1,34 @@
+package cn.edu.bit.newnewcc.backend.asm.optimizer;
+
+import cn.edu.bit.newnewcc.backend.asm.instruction.AsmAdd;
+import cn.edu.bit.newnewcc.backend.asm.instruction.AsmInstruction;
+import cn.edu.bit.newnewcc.backend.asm.instruction.AsmLoad;
+import cn.edu.bit.newnewcc.backend.asm.operand.IntRegister;
+import cn.edu.bit.newnewcc.backend.asm.operand.Register;
+
+import java.util.List;
+
+public class AddX0ToMv implements Optimizer {
+    @Override
+    public boolean runOn(List<AsmInstruction> instrList) {
+        boolean madeChange = false;
+
+        for (int i = 0; i < instrList.size(); ++i) {
+            if (instrList.get(i) instanceof AsmAdd addInstr) {
+                var opcode = addInstr.getOpcode();
+                if (opcode == AsmAdd.Opcode.ADD || opcode == AsmAdd.Opcode.ADDW) {
+                    if (addInstr.getOperand(2).equals(IntRegister.ZERO)) {
+                        instrList.set(i, new AsmLoad((Register) addInstr.getOperand(1), addInstr.getOperand(3)));
+                        madeChange = true;
+                    }
+                    if (addInstr.getOperand(3).equals(IntRegister.ZERO)) {
+                        instrList.set(i, new AsmLoad((Register) addInstr.getOperand(1), addInstr.getOperand(2)));
+                        madeChange = true;
+                    }
+                }
+            }
+        }
+
+        return madeChange;
+    }
+}
