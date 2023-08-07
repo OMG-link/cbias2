f107188a6b4236dd44091167cb62a7b7059db9bd
wtz <wangtz0607@outlook.com>
2023-08-07 17:46:23 +0800

    feat: 启用优化器

--- src/main/java/cn/edu/bit/newnewcc/backend/asm/optimizer/OptimizerManager.java
+++ src/main/java/cn/edu/bit/newnewcc/backend/asm/optimizer/OptimizerManager.java
@@ -1,11 +1,15 @@
 package cn.edu.bit.newnewcc.backend.asm.optimizer;
 
 import cn.edu.bit.newnewcc.backend.asm.instruction.AsmInstruction;
-
 import java.util.List;
 
 public class OptimizerManager {
     public void runOn(List<AsmInstruction> instrList) {
-
+        new LI0ToX0Optimizer().runOn(instrList);
+        new AddX0ToMvOptimizer().runOn(instrList);
+        new StrengthReductionOptimizer().runOn(instrList);
+        new BranchEliminationOptimizer().runOn(instrList);
+        new DeadInstructionEliminationOptimizer().runOn(instrList);
+        new DeadBlockEliminationOptimizer().runOn(instrList);
     }
 }
