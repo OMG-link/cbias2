cfb4c55fed5eb07c4ff48d5cdc35babf3071fd4b
fallleaves01 <adoubiq@qq.com>
2023-08-08 16:19:28 +0800

    feat:更新了图着色分配器的color函数，现在可以在含有物理寄存器的情况下进行着色

--- src/main/java/cn/edu/bit/newnewcc/backend/asm/controller/GraphColoringRegisterControl.java
+++ src/main/java/cn/edu/bit/newnewcc/backend/asm/controller/GraphColoringRegisterControl.java
@@ -4,8 +4,10 @@ import cn.edu.bit.newnewcc.backend.asm.AsmFunction;
 import cn.edu.bit.newnewcc.backend.asm.allocator.StackAllocator;
 import cn.edu.bit.newnewcc.backend.asm.instruction.AsmInstruction;
 import cn.edu.bit.newnewcc.backend.asm.instruction.AsmLabel;
+import cn.edu.bit.newnewcc.backend.asm.instruction.AsmMove;
 import cn.edu.bit.newnewcc.backend.asm.operand.IntRegister;
 import cn.edu.bit.newnewcc.backend.asm.operand.Register;
+import cn.edu.bit.newnewcc.backend.asm.util.AsmInstructions;
 import cn.edu.bit.newnewcc.backend.asm.util.Registers;
 import cn.edu.bit.newnewcc.ir.value.BasicBlock;
 import cn.edu.bit.newnewcc.pass.ir.structure.LoopForest;
@@ -20,10 +22,15 @@ public class GraphColoringRegisterControl extends RegisterControl {
     private List<Register> registers, physicRegisters;
     private final List<LifeTimeInterval> intervals = new ArrayList<>();
     private final Map<Register, Set<Register>> edges = new HashMap<>();
+    private final Map<Register, Set<Register>> coalesceEdges = new HashMap<>();
     private final Map<Register, Register> physicRegisterMap = new HashMap<>();
     private final Map<Register, Integer> spillCost = new HashMap<>();
     private final static int inf = 0x3f3f3f3f;
 
+    /**
+     * 获取寄存器被spill到内存中的代价，物理寄存器的代价总是inf*2，保证不能被spill
+     * @param instructionList 作为代价计算依据的指令列表
+     */
     void getRegisterCost(List<AsmInstruction> instructionList) {
         var irFunction = function.getBaseFunction();
         var loopForest = LoopForest.buildOver(irFunction);
@@ -62,12 +69,40 @@ public class GraphColoringRegisterControl extends RegisterControl {
         }
     }
 
+    /**
+     * 添加双向干涉边，存在干涉边的两个变量不能存在可并边
+     * @param u 边节点
+     * @param v 边节点
+     */
+    void addEdge(Register u, Register v) {
+        coalesceEdges.get(u).remove(v);
+        coalesceEdges.get(v).remove(u);
+        edges.get(u).add(v);
+        edges.get(v).add(u);
+    }
+
+    /**
+     * 在不存在干涉边的情况下添加可并干涉边
+     * @param u 边节点
+     * @param v 边节点
+     */
+    void addCoalesceEdge(Register u, Register v) {
+        if (!edges.get(u).contains(v)) {
+            coalesceEdges.get(u).add(v);
+            coalesceEdges.get(v).add(u);
+        }
+    }
+
+    /**
+     * 为虚拟寄存器和代码中含有的可变物理寄存器建立干涉图
+     */
     void buildGraph() {
         edges.clear();
         intervals.clear();
         for (var reg : registers) {
             intervals.addAll(function.getLifeTimeController().getInterval(reg));
             edges.put(reg, new HashSet<>());
+            coalesceEdges.put(reg, new HashSet<>());
         }
         Collections.sort(intervals);
         Set<LifeTimeInterval> activeSet = new HashSet<>();
@@ -76,25 +111,40 @@ public class GraphColoringRegisterControl extends RegisterControl {
             for (var last : activeSet) {
                 var ru = now.reg;
                 var rv = last.reg;
-                edges.get(ru).add(rv);
-                edges.get(rv).add(ru);
+                var defInst = now.range.a.getSourceInst();
+                if (defInst instanceof AsmMove asmMove && AsmInstructions.getMoveReg(asmMove).b.equals(rv)) {
+                    addCoalesceEdge(ru, rv);
+                } else {
+                    addEdge(ru, rv);
+                }
             }
             activeSet.add(now);
         }
     }
 
+    /**
+     * 为干涉图中的所有虚拟寄存器进行染色操作，若成功则结果保存与physicRegisterMap中
+     * @return 若成功染色返回true，否则返回false
+     */
     boolean color() {
         physicRegisterMap.clear();
         Map<Register, Integer> degree = new HashMap<>();
         for (var x : edges.keySet()) {
+            edges.get(x).addAll(coalesceEdges.get(x));
             degree.put(x, edges.get(x).size());
         }
         Queue<Register> queue = new ArrayDeque<>();
         Set<Register> inQueue = new HashSet<>();
+        int virtualRegCnt = 0;
         for (var x : edges.keySet()) {
-            if (degree.get(x) < physicRegisters.size()) {
-                queue.add(x);
-                inQueue.add(x);
+            if (x.isVirtual()) {
+                virtualRegCnt += 1;
+                if (degree.get(x) < physicRegisters.size()) {
+                    queue.add(x);
+                    inQueue.add(x);
+                }
+            } else {
+                physicRegisterMap.put(x, x);
             }
         }
         Stack<Register> stack = new Stack<>();
@@ -104,14 +154,14 @@ public class GraphColoringRegisterControl extends RegisterControl {
             for (var u : edges.get(v)) {
                 degree.put(u, degree.get(u) - 1);
                 if (degree.get(u) < physicRegisters.size()) {
-                    if (!inQueue.contains(u)) {
+                    if (u.isVirtual() && !inQueue.contains(u)) {
                         queue.add(u);
                         inQueue.add(u);
                     }
                 }
             }
         }
-        if (stack.size() < registers.size()) {
+        if (stack.size() < virtualRegCnt) {
             return false;
         }
         while (!stack.empty()) {
@@ -138,6 +188,7 @@ public class GraphColoringRegisterControl extends RegisterControl {
      * @return 分配后的指令列表
      */
     public List<AsmInstruction> allocatePhysicalRegisters(List<AsmInstruction> instructionList) {
+        getRegisterCost(instructionList);
         buildGraph();
         return instructionList;
     }
