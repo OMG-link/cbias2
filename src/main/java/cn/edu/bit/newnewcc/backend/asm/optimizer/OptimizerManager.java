c8234d59ac94398020101eca22d31d04bbd13be6
wtz <wangtz0607@outlook.com>
2023-08-08 21:06:49 +0800

    feat: 调整优化器执行顺序

--- src/main/java/cn/edu/bit/newnewcc/backend/asm/optimizer/OptimizerManager.java
+++ src/main/java/cn/edu/bit/newnewcc/backend/asm/optimizer/OptimizerManager.java
@@ -8,8 +8,8 @@ public class OptimizerManager {
         new LI0ToX0Optimizer().runOn(instrList);
         new AddX0ToMvOptimizer().runOn(instrList);
         new StrengthReductionOptimizer().runOn(instrList);
-        new BranchEliminationOptimizer().runOn(instrList);
         new DeadInstructionEliminationOptimizer().runOn(instrList);
+        new BranchEliminationOptimizer().runOn(instrList);
         new DeadBlockEliminationOptimizer().runOn(instrList);
     }
 }
