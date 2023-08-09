2bf05f1c614bdb650c1108fefabdb154cb243110
OMG-link <50469469+OMG-link@users.noreply.github.com>
2023-08-10 00:32:22 +0800

    feat: 增加错误检查代码

--- src/main/java/cn/edu/bit/newnewcc/pass/ir/GlobalCodeMotionPass.java
+++ src/main/java/cn/edu/bit/newnewcc/pass/ir/GlobalCodeMotionPass.java
@@ -143,10 +143,8 @@ public class GlobalCodeMotionPass {
         for (BasicBlock basicBlock : functionBasicBlocks) {
             for (Instruction instruction : basicBlock.getInstructions()) {
                 if (!isFixedInstruction(instruction) && successors.get(instruction).size() == 0) {
-                    // 没用的指令，直接扔了
-                    // 即便使用了DCE，此代码依然可能被触发
-                    // 例如 LoopForest 构建的过程中会涉及指令的标准化，可能导致旧指令不再被使用
-                    instruction.waste();
+                    // 没用的指令，理论上应该已经被DCE清理了
+                    throw new CompilationProcessCheckFailedException("Unused code found.");
                 }
             }
         }
