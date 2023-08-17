4a16ce6036374cfa755da370d6bebeeaf9de486c
OMG-link <50469469+OMG-link@users.noreply.github.com>
2023-08-17 17:53:25 +0800

    fix: 修复导致死循环的bug

--- src/main/java/cn/edu/bit/newnewcc/backend/asm/optimizer/ssabased/LI0ToX0Optimizer.java
+++ src/main/java/cn/edu/bit/newnewcc/backend/asm/optimizer/ssabased/LI0ToX0Optimizer.java
@@ -14,7 +14,7 @@ public class LI0ToX0Optimizer implements ISSABasedOptimizer {
         for (int id : AsmInstructions.getReadRegId(instruction)) {
             var operand = instruction.getOperand(id);
             if (!(operand instanceof Register)) continue;
-            if (helper.isConstIntOperand(operand) && helper.getConstIntValueFromOperand(operand) == 0) {
+            if (helper.isConstIntOperand(operand) && helper.getConstIntValueFromOperand(operand) == 0 && !IntRegister.ZERO.equals(operand)) {
                 instruction.setOperand(id, IntRegister.ZERO);
                 hasResult = true;
             }
